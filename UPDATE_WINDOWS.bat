e9b76efb30d358a22765ded70968d60e1f70e149
Phera aka sunflower <personal.phera@gmail.com>
2026-09-16 01:42:28 -0500

    fix(windows): make update wrapper policy-independent

--- UPDATE_WINDOWS.bat
+++ UPDATE_WINDOWS.bat
@@ -1,4 +1,4 @@
 @echo off
 setlocal
-powershell.exe -NoLogo -NoProfile -File "%~dp0scripts\windows\update.ps1" %*
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\windows\update.ps1" %*
 exit /b %ERRORLEVEL%
