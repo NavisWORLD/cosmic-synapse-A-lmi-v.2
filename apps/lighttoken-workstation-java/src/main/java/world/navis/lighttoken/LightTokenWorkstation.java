a789762bc8ef9ad92ab8d557fa9b7ddb8d60ab4d
Phera aka sunflower <personal.phera@gmail.com>
2026-09-18 19:30:05 -0500

    feat(lighttoken): add functional JavaFX resonance explorer

---
+++ apps/lighttoken-workstation-java/src/main/java/world/navis/lighttoken/LightTokenWorkstation.java
@@ -0,0 +1,9 @@
+package world.navis.lighttoken;
+
+public final class LightTokenWorkstation {
+    private LightTokenWorkstation() {}
+
+    public static void main(String[] args) {
+        LightTokenWorkstationApp.launchApp(args);
+    }
+}
